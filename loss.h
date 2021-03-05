02ff56d0603a423f00f22f2a6bde4a4dd2d8936b
jklp2 <954587302@qq.com>
2021-03-06 02:33:10 +0800

    定义基本loss

---
+++ loss.h
@@ -0,0 +1,30 @@
+//
+// Created by 15010 on 2021/3/6.
+//
+
+#ifndef PYTORCH_V4_LOSS_H
+#define PYTORCH_V4_LOSS_H
+#include "net.h"
+
+class Loss:public net{
+
+};
+class MSELOSS{
+public:
+    tensor* forward(vector<vector<tensor*>> x, vector<vector<tensor*>> y){
+        tensor* ret=NULL;
+        int m=x.size(),n=x[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!ret)
+                    ret=sq(su(x[i][j],y[i][j]));
+                else
+                    ret=ad(ret,sq(su(x[i][j],y[i][j])));
+            }
+        }
+        ret = di(ret,m*n);
+        loss_init(ret);
+        return ret;
+    }
+};
+#endif //PYTORCH_V4_LOSS_H
